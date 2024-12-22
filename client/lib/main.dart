8a3162f4761e72fb1da006a174ef70b2619984e8
Jerry Britto <jbritto2004@gmail.com>
2024-12-22 22:51:46 +0530

    setup done for firebase

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:client/firebase_options.dart';
 import 'package:client/screens/auth/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
